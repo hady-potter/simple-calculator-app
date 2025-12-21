7fd008cc90e6e565875e225fc7d55de05701d3ce
hady-potter <hadym137@gmail.com>
2025-12-21 05:44:14 +0200

    first commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class CalculatorTest {
+
+	private static final Calculator calculator = new Calculator();
+
+	@Test
+	public void addTest(){
+		int res = calculator.add(1, 2);
+		assertEquals(res, 3);
+	}
+	@Test
+	public void subtractTest(){
+		int res = calculator.subtract(1, 2);
+		assertEquals(res, -1);
+	}
+	@Test
+	public void multiplyTest(){
+		int res = calculator.multiply(1, 2);
+		assertEquals(res, 2);
+	}
+	@Test
+	public void divideTest(){
+		int res = calculator.divide(10, 2);
+		assertEquals(res, 5);
+	}
+
+}
